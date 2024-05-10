bd97be066a67fd24baddc8671f46426bfdff6fba
svetsky <166009586+svetsky@users.noreply.github.com>
2024-05-10 17:56:02 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿/*
+    Лабораторная №15 (4), Базовый поток
+    “Многообразие фигур”
+
+    Задание: Отобразить на экране n кругов (вариант 1).
+*/
+
+#include <SFML/Graphics.hpp>
+#include <Circle.hpp>
+#include <Game.hpp>
+
+int main()
+{
+    is::Game game(1600, 1000, "Game!!!");
+    game.Setup(10);
+    game.LifeCycle();
+
+    return 0;
+}
